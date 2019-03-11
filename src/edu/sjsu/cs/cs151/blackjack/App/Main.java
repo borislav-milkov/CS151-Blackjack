8e6b9d239ee185a36a7bd05f97df55d449d71aeb
AMilkov <milkovborislav@gmail.com>
2019-03-11 14:53:18 -0700

    example commit

--- src/edu/sjsu/cs/cs151/blackjack/App/Main.java
+++ src/edu/sjsu/cs/cs151/blackjack/App/Main.java
@@ -3,8 +3,8 @@ package edu.sjsu.cs.cs151.blackjack.App;
 public class Main {
 
 	public static void main(String[] args) {
-		Deck deck_shuff = new Deck();
-		System.out.println(deck_shuff);
+		Card card = new Card(Card.Suit.CLUBS, Card.Rank.ACE);
+		System.out.println(card);
 	}
 
 }
