e1dec8a1294ff7395c5f5d8f9726ed8ff1e59f80
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-04-22 21:39:50 -0700

    Added icon to TablePrototype

--- src/edu/sjsu/cs/cs151/blackjack/View/TablePrototype.java
+++ src/edu/sjsu/cs/cs151/blackjack/View/TablePrototype.java
@@ -1,16 +1,22 @@
 package edu.sjsu.cs.cs151.blackjack.View;
 
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Label;
 
-public class GameTable {
+// NOTE
+// Should probably implement this using Javax.swing instead of eclipse.swt
+
+
+public class TablePrototype {
 
 	protected Shell shell;
 	private Text text;
+	private Display display;
 
 	/**
 	 * Launch the application.
@@ -18,7 +24,7 @@ public class GameTable {
 	 */
 	public static void main(String[] args) {
 		try {
-			GameTable window = new GameTable();
+			TablePrototype window = new TablePrototype();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -29,7 +35,7 @@ public class GameTable {
 	 * Open the window.
 	 */
 	public void open() {
-		Display display = Display.getDefault();
+		display = Display.getDefault();
 		createContents();
 		shell.open();
 		shell.layout();
@@ -46,18 +52,20 @@ public class GameTable {
 	protected void createContents() {
 		shell = new Shell();
 		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		Image img = new Image(display, this.getClass().getResourceAsStream("/cards_Icon.png"));
+		shell.setImage(img);
+		shell.setText("BlackJack");
 		
 		Button btnNewButton = new Button(shell, SWT.NONE);
 		btnNewButton.setBounds(0, 55, 87, 33);
-		btnNewButton.setText("New Button");
+		btnNewButton.setText("HIT");
 		
 		Button button = new Button(shell, SWT.NONE);
-		button.setText("New Button");
+		button.setText("STAY");
 		button.setBounds(0, 87, 87, 33);
 		
 		Button button_1 = new Button(shell, SWT.NONE);
-		button_1.setText("New Button");
+		button_1.setText("BET");
 		button_1.setBounds(0, 119, 87, 33);
 		
 		text = new Text(shell, SWT.BORDER);
@@ -65,26 +73,26 @@ public class GameTable {
 		
 		Label lblNewLabel = new Label(shell, SWT.NONE);
 		lblNewLabel.setBounds(186, 184, 56, 71);
-		lblNewLabel.setText("New Label");
+		lblNewLabel.setText("CARD");
 		
 		Label label = new Label(shell, SWT.NONE);
-		label.setText("New Label");
+		label.setText("CARD");
 		label.setBounds(248, 184, 56, 71);
 		
 		Label label_1 = new Label(shell, SWT.NONE);
-		label_1.setText("New Label");
+		label_1.setText("CARD");
 		label_1.setBounds(124, 184, 56, 71);
 		
 		Label label_2 = new Label(shell, SWT.NONE);
-		label_2.setText("New Label");
+		label_2.setText("CARD");
 		label_2.setBounds(186, 10, 56, 71);
 		
 		Label label_3 = new Label(shell, SWT.NONE);
-		label_3.setText("New Label");
+		label_3.setText("CARD");
 		label_3.setBounds(248, 10, 56, 71);
 		
 		Label label_4 = new Label(shell, SWT.NONE);
-		label_4.setText("New Label");
+		label_4.setText("CARD");
 		label_4.setBounds(124, 10, 56, 71);
 
 	}
