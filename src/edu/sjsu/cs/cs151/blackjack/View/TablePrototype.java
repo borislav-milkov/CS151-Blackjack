f77e3ad59c3e6611975f0602e526ec0bc8738c88
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-04-25 18:13:02 -0700

    Visual improvements, added comments

--- src/edu/sjsu/cs/cs151/blackjack/View/TablePrototype.java
+++ src/edu/sjsu/cs/cs151/blackjack/View/TablePrototype.java
@@ -1,19 +1,13 @@
 package edu.sjsu.cs.cs151.blackjack.View;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.border.*;
 
 public class TablePrototype {
 
-	private JFrame frame;
+	private JFrame frmBlackjack;
 
 	/**
 	 * Launch the application.
@@ -23,7 +17,7 @@ public class TablePrototype {
 			public void run() {
 				try {
 					TablePrototype window = new TablePrototype();
-					window.frame.setVisible(true);
+					window.frmBlackjack.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,27 +36,63 @@ public class TablePrototype {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frmBlackjack = new JFrame();
+		frmBlackjack.setTitle("Blackjack");
+		frmBlackjack.getContentPane().setBackground(new Color(0, 100, 0));
 		Image frameIcon = new ImageIcon(this.getClass().getResource("/cards_Icon.png")).getImage();
-		frame.setIconImage(frameIcon);
-		frame.setBounds(100, 100, 600, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		frmBlackjack.setIconImage(frameIcon);
+		frmBlackjack.setBounds(100, 100, 600, 400);
+		frmBlackjack.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmBlackjack.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		
-		
+		/*
+		 * Initialize button panel and add buttons.
+		 */
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		panel.setPreferredSize(new Dimension(10, 50));
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		frmBlackjack.getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("Double Down");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_2 = new JButton("Hit");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Stand");
 		panel.setLayout(new GridLayout(0, 3, 0, 0));
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		panel.add(btnNewButton_2);
-		
-		JButton btnNewButton_1 = new JButton("New button");
 		panel.add(btnNewButton_1);
-		
-		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton_2);
 		panel.add(btnNewButton);
+		
+		
+		/*
+		 * Initialize JMenu and add menu items.
+		 */
+		JMenuBar menuBar = new JMenuBar();
+		frmBlackjack.setJMenuBar(menuBar);
+		
+		JMenu mnGame = new JMenu("Game");
+		menuBar.add(mnGame);
+		
+		JMenuItem mntmRestart = new JMenuItem("Restart");
+		mnGame.add(mntmRestart);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmInstructions = new JMenuItem("Instructions");
+		mnHelp.add(mntmInstructions);
+		
+		JMenuItem mntmTipsTricks = new JMenuItem("Tips & Tricks");
+		mnHelp.add(mntmTipsTricks);
 	}
 
 }
