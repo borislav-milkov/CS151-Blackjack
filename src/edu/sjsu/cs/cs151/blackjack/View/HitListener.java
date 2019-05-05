26c961a1a8c856bc857262199b8de9d434b3682e
AMilkov <milkovborislav@gmail.com>
2019-05-05 01:25:12 -0700

    added docs

--- src/edu/sjsu/cs/cs151/blackjack/View/HitListener.java
+++ src/edu/sjsu/cs/cs151/blackjack/View/HitListener.java
@@ -8,7 +8,8 @@ import edu.sjsu.cs.cs151.blackjack.Controller.HitMessage;
 import edu.sjsu.cs.cs151.blackjack.Controller.Message;
 import edu.sjsu.cs.cs151.blackjack.Controller.StandMessage;
 /**
- * The listener to listen for the click on the 'hit' button
+ * The listener to listen for the click on the 'hit' button. 
+ * The message queue is passed from the View object
  * */
 public class HitListener implements ActionListener {
 
