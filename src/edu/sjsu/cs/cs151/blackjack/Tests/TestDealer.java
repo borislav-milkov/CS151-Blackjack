94202b116d66b6e0f2e1b9f2924ccc021c25d723
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-05-10 14:55:20 -0700

    Implemented the remaining unit tests

--- src/edu/sjsu/cs/cs151/blackjack/Tests/TestDealer.java
+++ src/edu/sjsu/cs/cs151/blackjack/Tests/TestDealer.java
@@ -1,27 +1,56 @@
 package edu.sjsu.cs.cs151.blackjack.Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.*;
 
+import edu.sjsu.cs.cs151.blackjack.Model.Card;
 import edu.sjsu.cs.cs151.blackjack.Model.Dealer;
+import edu.sjsu.cs.cs151.blackjack.Model.Deck;
 import edu.sjsu.cs.cs151.blackjack.Model.Player;
 
-//TODO: Implement Dealer test
-class TestDealer {
+/**
+ * Tests the Dealer class.
+ */
+public class TestDealer {
 	Dealer testDealer;
+	Deck testDeck;
 	List<Player> testPlayers;
 
 	@Before
-	void setUp() throws Exception {
-		
+	public void setUp() throws Exception {
+		testDealer = new Dealer();
+		testPlayers = new ArrayList<Player>();
+		testDeck = new Deck();
+		testPlayers.add(new Player("hitMe"));
+		testPlayers.add(new Player("hitMe"));
+		testPlayers.add(new Player("hitMe"));
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testDeal() {
+		for(Player p : testPlayers) {
+			assertEquals(true, p.getHandValue() == 0);	// all hands are empty before dealing
+			testDealer.dealCards(testDeck, p, 1);
+			assertEquals(true, p.getHandValue() > 0);	// all hands are not empty after dealing
+		}
+	}
+	
+	@Test
+	public void testHit() {
+		// Dealer should hit on an empty hand
+		assertEquals(true, testDealer.getHandValue() == 0);
+		assertEquals(true, testDealer.willHit());
+		// Dealer should hit on 10
+		testDealer.addToHand(new Card("DIAMONDS", 10));
+		assertEquals(true, testDealer.willHit());
+		// Dealer should not hit on 17
+		testDealer.addToHand(new Card("SPADES", 7));
+		assertEquals(false, testDealer.willHit());
 	}
 
 }
--- src/edu/sjsu/cs/cs151/blackjack/Tests/TestPlayer.java
+++ src/edu/sjsu/cs/cs151/blackjack/Tests/TestPlayer.java
@@ -1,19 +1,47 @@
 package edu.sjsu.cs.cs151.blackjack.Tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
 import org.junit.*;
 
-//TODO: Implement Player test
-class TestPlayer {
+import edu.sjsu.cs.cs151.blackjack.Model.Card;
+import edu.sjsu.cs.cs151.blackjack.Model.Player;
+import edu.sjsu.cs.cs151.blackjack.Model.Pot;
 
+/**
+ * Tests the player class.
+ */
+public class TestPlayer {
+	Player testPlayer;
+	Pot testPot;
+	
 	@Before
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
+		testPlayer = new Player("test");
+		testPot = new Pot();
+		
+		testPlayer.addToHand(new Card("HEARTS", 5));
+		testPlayer.addToHand(new Card("CLUBS", 8));
+		testPlayer.addToHand(new Card("SPADES", 9));
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testBalance() {
+		// Check correct starting balance
+		assertEquals(1000, testPlayer.getChips());
+		testPlayer.putInPot(testPot, 400);
+		// Check balance is updating correctly
+		assertEquals(600, testPlayer.getChips());
+	}
+	
+	@Test
+	public void testHand() {
+		// Check correct hand value
+		assertEquals(22, testPlayer.getHandValue());
+		// Check bust functionality
+		assertEquals(true, testPlayer.isBust());
 	}
 
 }
