3cc00a2b542ab0d4843b47116f011ee6cd86e3ed
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-05-13 22:25:42 -0700

    Gambler interface docs

--- src/edu/sjsu/cs/cs151/blackjack/Model/Gambler.java
+++ src/edu/sjsu/cs/cs151/blackjack/Model/Gambler.java
@@ -16,17 +16,76 @@ public interface Gambler {
 	 */
 	public void displayHand();
 	
+	/**
+	 * Adds a card to players hand.
+	 * @param card	card to add
+	 */
 	public void addToHand(Card card);
+	
+	/**
+	 * Gets the hand of the player.
+	 * @return	hand of player
+	 */
 	public Hand getHand();
+	
+	/**
+	 * Gets the value of a players hand.
+	 * @return	sum of card values in player hand
+	 */
 	public int getHandValue();
+	
+	/**
+	 * Checks if the player has busted or not.
+	 * @return	true if the player has met conditions to bust
+	 * 			false if the player hasn't busted
+	 */
 	public boolean isBust();
+	
+	/**
+	 * Gets a string representation of player's name.
+	 * @return	"YOU" for a player
+	 * 			"DEALER" for a dealer
+	 */
 	public String getName();
+	
+	/**
+	 * Adds an amount to the active in-game pot.
+	 * @param pot	current in-game pot
+	 * @param value	amount to add into pot
+	 */
 	public void putInPot(Pot pot,int value);
+	
+	/**
+	 * The house collects the pot.
+	 * @param pot	current in-game pot
+	 */
 	public void winPot(Pot pot);
+	
+	/**
+	 * Checks if it's the current players turn.
+	 * @return	true if it is currently their turn
+	 */			
 	public boolean getTurn();
+	
+	/**
+	 * Gets the current balance of a player.
+	 * @return	remaining balance
+	 */
 	public int getChips();
+	
+	/**
+	 * Sets the balance of a player.
+	 * @param chips	 new balance to set
+	 */
 	public void setChips(int chips);
+	
+	/**
+	 * Empties the hand of the current player.
+	 */
 	public void clearHand();
 	
+	/**
+	 * Returns a string representation of this player.
+	 */
 	public String toString();
 }
